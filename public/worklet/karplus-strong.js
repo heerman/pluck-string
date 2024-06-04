108fd6a57a0c882a6d358bacdee9af8131c41cff
heerman <43624284+heerman@users.noreply.github.com>
2024-06-04 08:23:43 -0400

    setup karplus-strong durations

--- public/worklet/karplus-strong.js
+++ public/worklet/karplus-strong.js
@@ -9,32 +9,38 @@ class KarplusStrongProcessor extends AudioWorkletProcessor {
         super()
         this.elapsedTime = 0
         this.isBurstActive = true
+        this.isActive = true
     }
 
     process(inputs, outputs, parameters) {
         // karplus-strong is a noise burst into a feedback delay lpf
         const output = outputs[0]
-        const duration = parameters.duration.length > 1 ? parameters.duration : parameters.duration[0]
-        const sampleDuration = 1 / sampleRate
+        const noiseDur = 0.2 // seconds
+        const fullDur = 5 * noiseDur
+        const sampleDur = 1 / sampleRate
 
         // TODO implement the feedback delay lpf
         output.forEach(channel => {
             for (let i = 0; i < channel.length; i++) {
                 if (this.isBurstActive) {
-                    const amplitude = 1 - (this.elapsedTime / duration)
-                    channel[i] = (Math.random() * 2 - 1) * amplitude
-                    this.elapsedTime += sampleDuration
+                    const maxAmp = 0.25
+                    const ampEnv = 1 - (this.elapsedTime / noiseDur)
+                    channel[i] =  maxAmp * ampEnv * (Math.random() * 2 - 1)
 
-                    if (this.elapsedTime >= duration) {
-                        this.isBurstActive = false
-                    }
+                    if (this.elapsedTime >= noiseDur) this.isBurstActive = false
+
+                } else if (this.isActive) {
+                    channel[i] =  0.1 * (Math.random() * 2 - 1)
+                    if (this.elapsedTime >= fullDur) this.isActive = false
+
                 } else {
                     channel[i] = 0
                 }
+                this.elapsedTime += sampleDur
             }
         })
 
-        return this.isBurstActive
+        return this.isActive
     }
 }
 
